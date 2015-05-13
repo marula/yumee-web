17139b74ea0174d5f6eab288c755bc12aa098a41
Rick-KLN <rick@baobablogic.com>
2015-05-13 20:20:18 +0200

    add orders

--- app/models/item.js
+++ app/models/item.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
+  description: DS.attr('string'),
   price: DS.attr('number'),
   restaurant: DS.belongsTo('restaurant', { async: true })
 });
--- app/router.js
+++ app/router.js
@@ -10,18 +10,23 @@ export default Router.map(function() {
 
   this.route('restaurants', function() {
     this.route('new');
+
     this.route('restaurant', { path: '/:restaurant_id' }, function() {
       this.route('edit');
+
       this.route('items', function() {
         this.route('new');
         this.route('item', { path: '/:item_id' }, function() {
           this.route('edit');
         });
       });
+
+      this.route('orders', function() {
+        this.route('new');
+        this.route('order', { path: '/:order_id' }, function() {
+          this.route('edit');
+        });
+      });
     });
   });
-
-  this.route('orders', function() {});
-  this.route('comments', function() {});
-  this.route('users', function() {});
 });
