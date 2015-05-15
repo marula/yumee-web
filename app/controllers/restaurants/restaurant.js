cb350c86ae11339aa7a4e6c6b0abe9eb173fc5dd
Jaco Smit @Home <17523559@sun.ac.za>
2015-05-15 21:56:59 +0200

    Fixed orders bug, added delete to orders list.

--- app/controllers/restaurants/restaurant.js
+++ app/controllers/restaurants/restaurant.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  needs: ['application'],
   breadCrumb: function() {
     return this.get('model.name');
   }.property('model.name')
